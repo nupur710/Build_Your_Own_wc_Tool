4fbfeb071d57bea5b49005dfcd7a7baace7332c5
Nupur Singh <nupur.singh2410@gmail.com>
2024-02-24 01:00:25 +0530

    Improved countWords implementation

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,91 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    private static void commandLineOption(String ip, File file) {
+        String fileName= file.getName();
+        switch (ip) {
+            case "-c":
+               long x= countBytes(file);
+                System.out.println(x + " " + fileName);
+                break;
+            case "-l":
+                int l= countLines(file);
+                System.out.println(l + " " + fileName);
+                break;
+            case "-w":
+                int w= countWords(file);
+                System.out.println(w + " " + fileName);
+                break;
+            case "-m":
+                int m= countChars(file);
+                System.out.println(m + " " + fileName);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
         }
     }
+
+    private static int countWords(File file) {
+        int words= 0;
+        try {
+            BufferedReader br= new BufferedReader(new FileReader(file));
+            String line;
+            while((line = br.readLine()) != null) {
+                words+= countWordsInLine(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } return words;
+    }
+
+    private static int countWordsInLine(String line) {
+        if(line == null || line.isEmpty()) {
+            return 0;
+        } int wordCount= 0;
+        return 0;
+    }
+
+    private static int countLines(File file) {
+        int count= 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String lines;
+            while ((lines = br.readLine()) != null)
+                count++;
+        } catch (IOException e) {
+
+        } return count;
+    }
+
+    private static int countChars(File file) {
+        int b= 0;
+        int length= 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            while ((b = br.read()) != -1) {
+                length++;
+            }
+        }catch (IOException e) {
+
+        }
+        return length;
+    }
+    private static long countBytes(File fis) {
+        return fis.length();
+    }
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+        String userInput= scanner.nextLine();
+        String[] userInputArr= userInput.split(" ");
+        String fileName= userInputArr[2];
+        String cmd= userInputArr[1];
+        File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\"+fileName);
+        commandLineOption(cmd, file);
+    }
+
 }
