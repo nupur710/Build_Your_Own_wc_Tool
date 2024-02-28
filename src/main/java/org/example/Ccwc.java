bad41ec62af878db76bedaf064fb3afd0c9cbc6f
Nupur Singh <nupur.singh2410@gmail.com>
2024-02-29 01:01:51 +0530

    Improved program structure

---
+++ src/main/java/org/example/Ccwc.java
@@ -0,0 +1,78 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Ccwc {
+
+    private File file;
+    Ccwc(File file) {
+        this.file= file;
+    }
+
+    long countBytes() {
+        return file.length(); //returns length of file in bytes
+    }
+
+    int countChars() {
+        int b= 0;
+        int charCount= 0;
+        try {
+            BufferedReader br= new BufferedReader(new FileReader(file));
+            while((b=br.read())!= -1) { //br.read() reads each character of a file
+                charCount++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } return charCount;
+    }
+
+    int countLines() {
+        int lines= 0;
+        try {
+            BufferedReader br= new BufferedReader(new FileReader(file));
+            while(br.readLine() != null) { //br.readLine() reads text line by line
+                lines++;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } return lines;
+    }
+
+    //This method counts words in each line.
+    int countWords() {
+        //We specify character encoding of file
+        Charset charset= StandardCharsets.UTF_8;
+        int c; //current character being read
+        boolean inWord=  false; //currently in word?
+        int words= 0;
+        try {
+            BufferedReader br= new BufferedReader(new FileReader(file, charset));
+            //read each character in file
+            while((c= br.read())!= -1) {
+                //check if current character is a whitespace
+                if(Character.isWhitespace(c)) {
+                    if(inWord) inWord = false;
+                        //if the current character being read is a whitespace
+                        //and we were previously inside a word, set inWord to false
+                        //as we are out of word now
+                } else {
+                    //if the current character is not a whitespace, it is part of a word
+                    //if the previous character was a whitespace (inWord= false)- a new word is starting
+                    //we are starting a new word; set inWord= true
+                    if (!inWord) {
+                        inWord = true;
+                        //a new word
+                        words++;
+                    }
+                }
+            } return words;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } return -1;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,91 +1,54 @@
 package org.example;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
-    private static void commandLineOption(String ip, File file) {
+    private static void commandLineOption(String ip, File file) throws IOException {
+       Ccwc ccwc= new Ccwc(file);
         String fileName= file.getName();
         switch (ip) {
             case "-c":
-               long x= countBytes(file);
+               long x= ccwc.countBytes();
                 System.out.println(x + " " + fileName);
                 break;
             case "-l":
-                int l= countLines(file);
+                int l= ccwc.countLines();
                 System.out.println(l + " " + fileName);
                 break;
             case "-w":
-                int w= countWords(file);
+               // int w= countWordsInLine(file);
+                int w= ccwc.countWords();
                 System.out.println(w + " " + fileName);
                 break;
             case "-m":
-                int m= countChars(file);
+                int m= ccwc.countChars();
                 System.out.println(m + " " + fileName);
-
+                break;
+            case " ":
+                System.out.println(ccwc.countLines() + " " + ccwc.countWords() + " " + ccwc.countBytes() + " " + fileName);
+                break;
         }
     }
 
-    private static int countWords(File file) {
-        int words= 0;
-        try {
-            BufferedReader br= new BufferedReader(new FileReader(file));
-            String line;
-            while((line = br.readLine()) != null) {
-                words+= countWordsInLine(line);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException i) {
-            i.printStackTrace();
-        } return words;
-    }
-
-    private static int countWordsInLine(String line) {
-        if(line == null || line.isEmpty()) {
-            return 0;
-        } int wordCount= 0;
-        return 0;
-    }
-
-    private static int countLines(File file) {
-        int count= 0;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String lines;
-            while ((lines = br.readLine()) != null)
-                count++;
-        } catch (IOException e) {
-
-        } return count;
-    }
-
-    private static int countChars(File file) {
-        int b= 0;
-        int length= 0;
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            while ((b = br.read()) != -1) {
-                length++;
-            }
-        }catch (IOException e) {
-
-        }
-        return length;
-    }
-    private static long countBytes(File fis) {
-        return fis.length();
-    }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner scanner= new Scanner(System.in);
         String userInput= scanner.nextLine();
         String[] userInputArr= userInput.split(" ");
-        String fileName= userInputArr[2];
-        String cmd= userInputArr[1];
-        File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\"+fileName);
-        commandLineOption(cmd, file);
+        if(userInputArr.length == 3) {
+            String fileName = userInputArr[2];
+            String cmd = userInputArr[1];
+            File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\"+fileName);
+            commandLineOption(cmd, file);
+        } else if (userInputArr.length == 2) {
+            String fileName= userInputArr[1];
+            String cmd= " ";
+            File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\"+fileName);
+            commandLineOption(cmd, file);
+        }
+
     }
 
 }
